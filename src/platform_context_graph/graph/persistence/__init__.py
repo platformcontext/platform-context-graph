ccd0fd34b3cf0f0e0b466263ed91a1f83346be52
Allen Sanabria <asanabria6910@gmail.com>
2026-04-02 17:40:53 -0400

    refactor: move repository persistence helpers into graph package

--- src/platform_context_graph/graph/persistence/__init__.py
+++ src/platform_context_graph/graph/persistence/__init__.py
@@ -11,13 +11,29 @@ from .directories import (
     merge_directory_chain,
 )
 from .entities import build_entity_merge_statement
+from .repositories import (
+    _bounded_positive_int_config,
+    _merge_directory_chain,
+    _relative_path_with_fallback,
+    _run_managed_write,
+    _run_write_query,
+    add_repository_to_graph,
+    read_repository_metadata,
+)
 
 __all__ = (
+    "_bounded_positive_int_config",
+    "_merge_directory_chain",
+    "_relative_path_with_fallback",
+    "_run_managed_write",
+    "_run_write_query",
     "build_entity_merge_statement",
     "collect_directory_chain_rows",
     "delete_file_from_graph",
     "delete_repository_from_graph",
     "flush_directory_chain_rows",
     "merge_directory_chain",
+    "add_repository_to_graph",
+    "read_repository_metadata",
     "update_file_in_graph",
 )
